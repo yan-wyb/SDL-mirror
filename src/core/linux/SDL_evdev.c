4c5bd56492e0f80dccbe4e52e20bfc5873835476
Jools Wills <buzz@exotica.org.uk>
2016-04-22 20:36:23 +0100

    silence log output for unknown input

--- src/core/linux/SDL_evdev.c
+++ src/core/linux/SDL_evdev.c
@@ -712,9 +712,11 @@ SDL_EVDEV_translate_keycode(int keycode)
     if (keycode < SDL_arraysize(EVDEV_Keycodes)) {
         scancode = EVDEV_Keycodes[keycode];
     }
+    /*
     if (scancode == SDL_SCANCODE_UNKNOWN) {
         SDL_Log("The key you just pressed is not recognized by SDL. To help get this fixed, please report this to the SDL mailing list <sdl@libsdl.org> EVDEV KeyCode %d \n", keycode);
     }
+    */
     return scancode;
 }
 
