204b25b0ffb6fd3d255b7784f96b4c44b31943de
Conn O'Griofa <connogriofa@gmail.com>
2019-08-07 03:11:08 +0100

    Improve kmsdrm + rpi driver coexistence
    * Ensure that DEFAULT_OGL is set correctly (fixes OpenGL)
    * Silence kmsdrm driver init error when using VideoCore.
    * Remove VideoCore cflags from SDL_CFLAGS; these flags are unnecessary
      and will cause conflicts when building SDL2 applications for VC4.

--- src/video/kmsdrm/SDL_kmsdrmvideo.c
+++ src/video/kmsdrm/SDL_kmsdrmvideo.c
@@ -124,6 +124,12 @@ static unsigned int get_dricount(void)
     DIR *folder;
 
     if (!(stat(KMSDRM_DRI_PATH, &sb) == 0 && S_ISDIR(sb.st_mode))) {
+#if SDL_VIDEO_DRIVER_RPI
+        // exit silently if VC4 driver is not active
+        SDL_bool vc4 = (0 == access("/sys/module/vc4/", F_OK));
+        if (!vc4)
+            return 0;
+#endif
         SDL_SetError("The path %s cannot be opened or is not available",
         KMSDRM_DRI_PATH);
         return 0;
